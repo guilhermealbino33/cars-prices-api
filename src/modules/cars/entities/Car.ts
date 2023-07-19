eff4e4d4cc90236c06ceea93ff7410f356143aae
Guilherme Albino <guilhermealbino33@gmail.com>
2023-07-19 13:48:31 -0300

    feat: add car entity

---
+++ src/modules/cars/entities/Car.ts
@@ -0,0 +1,32 @@
+export interface CarProps {
+  id?: string;
+  manufacturer: string;
+  model: string;
+  modelYear: string;
+  licensePlate: string;
+  code: string;
+  msrp: string;
+  deletedAt?: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class Car {
+  private props: CarProps;
+
+  constructor(props: CarProps) {
+    this.props = props;
+  }
+
+  get getProps() {
+    return { ...this.props };
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  get deletedAt() {
+    return this.props.deletedAt;
+  }
+}
